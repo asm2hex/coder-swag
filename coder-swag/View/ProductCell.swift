d76f9ef1859e2e04951ec648d31913112757a3dc
Lancelot Edwards <lancelot.edwards@gmail.com>
2017-08-10 11:27:37 -0500

    design collection view ui

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Lancelot Edwards on 10/08/2017.
+//  Copyright © 2017 Lancelot Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+  @IBOutlet weak var produceImageView: UIImageView!
+  @IBOutlet weak var productTitleLabel: UILabel!
+  @IBOutlet weak var productPriceLabel: UILabel!
+}
