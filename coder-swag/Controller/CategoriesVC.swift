0d48fb9dd851d36c12cadce68199c59073929dfe
Lancelot Edwards <lancelot.edwards@gmail.com>
2017-08-09 20:56:27 -0500

    design initial screen and include assets

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CategoriesVC.swift
 //  coder-swag
 //
 //  Created by Lancelot Edwards on 09/08/2017.
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
 
+  @IBOutlet weak var categoryTableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Lancelot Edwards on 09/08/2017.
+//  Copyright © 2017 Lancelot Edwards. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+  @IBOutlet weak var categoryImageView: UIImageView!
+  @IBOutlet weak var categoryTitleLabel: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
